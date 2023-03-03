c42ea634bf20c0c7f41e9108b639ab5e09850607
TroyBou2 <126248943+TroyBou2@users.noreply.github.com>
2023-03-03 14:03:01 +0300

    Add files via upload

---
+++ Задание3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} Чётное");
+}
+else 
+{
+    Console.WriteLine($"{num} Нечётное");
+}
