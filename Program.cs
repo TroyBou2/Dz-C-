d4e34c4778050bea278cf7a7591ce6d58afe6277
unknown <bou.troy1324@mail.ru>
2023-03-03 13:53:21 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Большее число: {a}");
+    Console.WriteLine($"Меньшее число: {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Большее число: {b}");
+    Console.WriteLine($"Меньшее число: {a}");
+    
+} else 
+{
+    Console.WriteLine("Числа равны");
+}
