d72df06c96b91abbab14d276e026edbdb03b16d5
TroyBou2 <126248943+TroyBou2@users.noreply.github.com>
2023-04-01 09:57:27 +0300

    Add files via upload

---
+++ задание66/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите перове число");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int m, int n)
+{
+    if (m == n)
+        return m;
+    return m + SumNumbers(m + 1, n);
+
+}
+System.Console.WriteLine($"Сумма натуральных чисел в диапозоне от {m} до {n} равна {SumNumbers(m, n)}");
---
+++ задание68/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите перове число");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ack(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Ack(m - 1, 1);
+    return Ack(m - 1, Ack(m, n - 1));
+
+
+}
+
+System.Console.WriteLine(Ack(m,n));
